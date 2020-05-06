b44a4bc32f75c1f4472a129425c90465235fa730
Jens <jdisselhoff@gmail.com>
2020-05-06 14:04:16 -0500

    Update Code

--- components/SignupForm.vue
+++ components/SignupForm.vue
@@ -4,24 +4,30 @@
       <b-form-input
         id="email"
         v-model="form.email"
-        :state="!errors.email && null"
+        :state="!$get(errors, userObject+'.email') && null"
         type="text"
         placeholder="Email address"
       />
-      <b-form-invalid-feedback v-if="$get(errors, 'email.blank')">Cannot be empty.</b-form-invalid-feedback>
-      <b-form-invalid-feedback v-else-if="$get(errors, 'email.invalid')">Please specify valid email.</b-form-invalid-feedback>
-      <b-form-invalid-feedback v-else-if="$get(errors, 'email.taken')">This email is already registered. <nuxt-link to="/login">Sign in</nuxt-link></b-form-invalid-feedback>
+      <b-form-invalid-feedback v-if="$get(errors, userObject+'.email.blank')">Cannot be empty.</b-form-invalid-feedback>
+      <b-form-invalid-feedback v-else-if="$get(errors, userObject+'.email.invalid')">Please specify valid email.</b-form-invalid-feedback>
+      <b-form-invalid-feedback v-else-if="$get(errors, userObject+'.email.taken')">This email is already registered. <nuxt-link to="/login">Sign in</nuxt-link></b-form-invalid-feedback>
     </b-form-group>
     <b-form-group>
       <b-form-input
         id="password"
         v-model="form.password"
-        :state="!errors.password && null"
+        :state="!$get(errors, 'user.password') && null"
         type="password"
         placeholder="Password"
       />
-      <b-form-invalid-feedback v-if="$get(errors, 'password.blank')">
-        This is a required field and must be at least 3 characters.
+      <b-form-invalid-feedback v-if="$get(errors, userObject+'.password.blank')">
+        This is a required field and must be at least 8 characters (blank).
+      </b-form-invalid-feedback>
+      <b-form-invalid-feedback v-if="$get(errors, userObject+'.password.too_short')">
+        This is a required field and must be at least 8 characters (too short).
+      </b-form-invalid-feedback>
+      <b-form-invalid-feedback v-if="$get(errors, userObject+'.password.invalid')">
+        This is a required field and must be at least 8 characters (invalid).
       </b-form-invalid-feedback>
     </b-form-group>
     <b-button type="submit" variant="success">
@@ -33,23 +39,33 @@
 
 <script>
 export default {
-  props: ['company', 'submit'],
+  props: ['company', 'submit', 'userObjectName'],
   data () {
     return {
       form: {
         email: '',
         password: ''
       },
-      errors: {}
+      errors: {
+      }
     }
+  },
+  computed: {
+
+    userObject () {
+      return this.userObjectName ? this.userObjectName : 'user';
+    }
+
   },
   methods: {
     async onSubmit () {
-      this.isValidationError = false
+      this.isValidationError = false;
+      console.log('submitted');
       try {
         await this.submit(this.form)
       } catch (error) {
         if (error.details) {
+          console.log('errorDetails', error.details)
           this.errors = error.details
         } else {
           throw error
@@ -60,7 +76,7 @@ export default {
 
 
   mounted() {
-    console.log('submit action', this.submit)
+    console.log('userObject', this.userObject)
   }
 }
 </script>
--- plugins/axios.js
+++ plugins/axios.js
@@ -3,9 +3,28 @@ import isEmpty from 'lodash.isempty'
 
 export default function (context) {
   let { $axios, redirect } = context
+
+
   $axios.onError(error => {
     let code = parseInt(error.response && error.response.status)
     if (code === 401) {
+
+      error.details = {}
+      let entities = error.response.data
+      for (let entity in entities) { // entity = "user"
+        error.details[entity] = {}
+        let railsDetails = entities[entity]
+        for (let prop in railsDetails) {
+          let codes = {}
+          set(error.details[entity], prop, codes)
+          for (let rule of railsDetails[prop]) {
+            let code = rule.error
+            delete rule.error
+            codes[code] = isEmpty(rule) || rule
+          }
+        }
+      }
+      console.log('error 401', error.details)
       redirect('/login', {
         // Append either `?back` or nothing
         back: context.from.name !== 'login' && null
@@ -26,6 +45,27 @@ export default function (context) {
           }
         }
       }
+      console.log('error 422', error.details)
+    } else if (code === 400) {
+      error.details = {}
+      let entities = error.response.data
+      console.log('error 400')
+      console.log(error.response.data)
+      for (let entity in entities) { // entity = "user"
+        error.details[entity] = {}
+        let railsDetails = entities[entity]
+        for (let prop in railsDetails) {
+          let codes = {}
+          set(error.details[entity], prop, codes)
+          for (let rule of railsDetails[prop]) {
+            let code = rule.error
+            delete rule.error
+            codes[code] = isEmpty(rule) || rule
+          }
+        }
+      }
+    } else if (code === 500) {
+      console.log('error 500', error.response.data)
     }
   })
 }
